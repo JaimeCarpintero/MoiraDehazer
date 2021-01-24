99e8e1aef3b0355c9768d694cba97a8e5d2669f2
Jaime Carpintero <jaime.carpintero@uabc.edu.mx>
2021-01-23 18:47:38 -0800

    Added .gitignore file and created base project

---
+++ MDehazer/MDehazer/Controllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MDehazer
+//
+//  Created by Jaime Carpintero Carrillo 23-Jan-2021
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
